20924b9eab54e8f0fdcf799d9e9dec20960c78c0
Himanshu Kabra <kabrahimanshu123@gmail.com>
2025-08-20 00:56:42 +0530

    setuo all pages and tailwind css

---
+++ educase/src/App.jsx
@@ -0,0 +1,12 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './pages/Home.jsx';
+
+function App() {
+  return(
+    <div>
+      <Home />
+    </div>
+  );
+}
+
+export default App;
